6d1b1642a807eeb88979dab48e62c12870312e58
north1013 <fandersson50@gmail.com>
2018-10-30 19:25:52 +0100

    python script remade in bash, added a file "files" for list

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 src="$PWD""/"
-dst="$HOME""/"
-files=$(cat list)
+dst="$HOME""/test/"
+files=$(cat files)
 
 for file in $files
 do
-	echo "$file"
+	echo -n "Do you want to overwrite $file"
+	read answer
+	if [ "$answer" != "${answer#[Yy]}" ] ;then
+		if [ -f "$file" ]; then
+			mkdir -p "$dst""$file"
+		fi
+		cp "$src""$file" "$dst""$file"
+	fi
 done
 
 

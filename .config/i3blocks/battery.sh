27ac684a22cc60f6cde96992f6c9202d6e03f627
north1013 <fandersson50@gmail.com>
2018-11-06 18:46:30 +0100

    battery will only show if battery exists

--- .config/i3blocks/battery.sh
+++ .config/i3blocks/battery.sh
@@ -6,17 +6,19 @@ GREEN="#A3BE8C"
 
 BAT=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
 STATE=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E state|xargs|cut -d' ' -f2)
+if [ $BAT ]
+then
+	echo "Battery: $STATE $BAT%"
+	echo "Bat: $BAT%"
 
-echo "Battery: $STATE $BAT%"
-echo "Bat: $BAT%"
-
-if [ $BAT -gt 95 ]
-then
-	echo "$GREEN"
-elif [ $BAT -lt 5 ]
-then
-	echo "$RED"
-elif [ $BAT -lt 20 ]
-then
-	echo "$YELLOW"
+	if [ $BAT -gt 95 ]
+	then
+		echo "$GREEN"
+	elif [ $BAT -lt 5 ]
+	then
+		echo "$RED"
+	elif [ $BAT -lt 20 ]
+	then
+		echo "$YELLOW"
+	fi
 fi
